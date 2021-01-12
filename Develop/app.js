77d30cca266045cf0e2def163be7e0560c7ba566
Sungpil An <ansungpil1@gmail.com>
2021-01-12 15:18:59 -0500

    Added inquirer for the intern class and created function to create and render teammembers

--- Develop/app.js
+++ Develop/app.js
@@ -102,56 +102,41 @@ function getEngineer() {
         let { name, id, email, github } = answers;
         let engineer = Engineer(name, id, email, github);
         teamMember.push(engineer);
-    })
+    });
 }
 // Function to create Intern
-function getEngineer() {
+function getIntern() {
     inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "What is the engineer's name?"
+            message: "What is the Intern's name?"
         },
         {
             type: "input",
             name: "id",
-            message: "What is the engineer's id?"
+            message: "What is the Intern's id?"
         },
         {
             type: "input",
             name: "email",
-            message: "What is the engineer's email?"
+            message: "What is the Intern's email?"
         },
         {
             type: "input",
-            name: "github",
-            message: "What is the engineer's github?"
+            name: "school",
+            message: "What is the Intern's school name?"
         },
     ]).then(answers => {
-        let { name, id, email, github } = answers;
-        let engineer = Engineer(name, id, email, github);
-        teamMember.push(engineer);
-    })
+        let { name, id, email, school } = answers;
+        let intern = Intern(name, id, email, school);
+        teamMember.push(intern);
+    });
 }
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// Function to build team
+function buildTeam() {
+    fs.writeFileSync(outputPath, render(teamMember), "utf-8");
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+mainApp()
